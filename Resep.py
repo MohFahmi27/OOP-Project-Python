2d913f1fd0243300b1b86c952632ccbefd3a9aee
Mohammad Fahmi <50865350+MohFahmi27@users.noreply.github.com>
2020-04-15 20:48:21 +0800

    update

--- Resep.py
+++ Resep.py
@@ -1,10 +1,10 @@
-# from Dokter import Dokter
-# from Pasien import Pasien
-# from Obat import Obat
+from Dokter import Dokter
+from Pasien import Pasien
+from Obat import Obat
 
-# class Resep:
+class Resep:
 
-#     def __init__(self, Dokter, Pasien, Obat = []):
-#         self.Dokter = Dokter
-#         self.Pasien = Pasien
-#         self.Obat = Obat
+     def __init__(self, Dokter, Pasien, Obat = []):
+         self.Dokter = Dokter
+         self.Pasien = Pasien
+         self.Obat = Obat
