5de8cc46aef8fe4f0da43162590e735dae79308d
MohFahmi27 <mohammadfahmi417@gmail.com>
2020-04-16 08:15:54 +0800

    Update User.py

--- User.py
+++ User.py
@@ -4,21 +4,21 @@ class User:
         self.__username = username
         self.__password = password
 
-    # @property
-    # def username(self):
-    #     return self.__username
+    @property
+    def username(self):
+        return self.__username
     
-    # @username.setter
-    # def username(self, username):
-    #     self.__username = username
+    @username.setter
+    def username(self, username):
+        self.__username = username
 
-    # @property
-    # def password(self):
-    #     return self.__password
+    @property
+    def password(self):
+        return self.__password
     
-    # @password.setter
-    # def password(self, password):
-    #     self.__password = password
+    @password.setter
+    def password(self, password):
+        self.__password = password
 
     def verifyUser(self, username, password) -> bool:
         pass
