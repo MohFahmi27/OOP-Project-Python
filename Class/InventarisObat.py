14ef4276fb98c2e3d5a399d0cab04fcd154f52ad
MohFahmi27 <mohammadfahmi417@gmail.com>
2020-04-27 08:21:32 +0800

    add dateExp into InventarisObat.py

--- Class/InventarisObat.py
+++ Class/InventarisObat.py
@@ -1,14 +1,17 @@
+from Class.JenisObat import JenisObat
 from Class.Obat import Obat
 from db.Orm.InventarisObatOrm import InventarisObatOrm
 from db.base import sessionFactory
+from datetime import date, timedelta
+from Class.Obat import Obat
 
 
 class InventarisObat():
 
-    def __init__(self, idObat, stockObat, expObat, hargaObat, lokasi):
+    def __init__(self, idObat, stockObat, hargaObat, lokasi):
         self.idobat = idObat
         self.stockObat = stockObat
-        self.expObat = expObat
+        self.expObat = (date.today()) + timedelta(weeks=26)
         self.hargaObat = hargaObat
         self.lokasi = lokasi
 
@@ -35,4 +38,10 @@ class InventarisObat():
         except Exception as e:
             print("===>", e)
 
+
+# paracetamol = Obat(JenisObat(1).name, "Paracetamol")
+# paracetamol.insertObat()
+# Obat.showObat()
+# inventarisObat1 = InventarisObat(2, 2, 50_000, "dirak nomor 2")
+# inventarisObat1.insertInventarisObat()
 # InventarisObat.showInventarisObat()
